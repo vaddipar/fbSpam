4ab1cad4b7f6c8a8bebea439b1f75ed335e575c3
Sandeep Srivastav Vaddiparthy <vsandeepsrivastav@gmail.com>
2018-07-25 00:50:11 +0530

    fetches wishe on a user's bday

--- src/main/java/com/vaddipar/fbspam/utilities/FBSpam.java
+++ src/main/java/com/vaddipar/fbspam/utilities/FBSpam.java
@@ -3,12 +3,13 @@ package com.vaddipar.fbspam.utilities;
 import org.json.JSONException;
 
 import javax.xml.ws.http.HTTPException;
+import java.text.ParseException;
 import java.util.List;
 
 public class FBSpam {
-    public static void main(String args[]) throws HTTPException, JSONException {
+    public static void main(String args[]) throws HTTPException, JSONException, ParseException {
         FacebookHelper fbHelper = new FacebookHelper();
-        List<String> postIds = fbHelper.getFeedOnDate("2016/07/21");
+        List<String> postIds = fbHelper.getBirthdayWishes();
         for (String postId : postIds) System.out.println(postId);
         System.out.println("I fetched "+postIds.size()+" posts");
     }
--- src/main/java/com/vaddipar/fbspam/utilities/FacebookHelper.java
+++ src/main/java/com/vaddipar/fbspam/utilities/FacebookHelper.java
@@ -17,10 +17,13 @@ import java.util.*;
 
 class FacebookHelper {
 
-    private String fbFeedGraphAPIURL;
+    private String fbMyGraphAPIURL;
+    DateFormat fbDateFormat;
 
     FacebookHelper() {
-        fbFeedGraphAPIURL = "https://graph.facebook.com/me/feed";
+        fbMyGraphAPIURL = "https://graph.facebook.com/v3.0/me";
+        fbDateFormat = new SimpleDateFormat("MM/dd/yyyy", Locale.ENGLISH);
+        fbDateFormat.setTimeZone(TimeZone.getDefault());
     }
 
     private void dateStringToDate(String dateString, MultivaluedMap queryParams){
@@ -28,11 +31,8 @@ class FacebookHelper {
         Given a string of the format YYYY/MM/DD. Convert it to a date object and return it.
          */
 
-        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd", Locale.ENGLISH);
-        dateFormat.setTimeZone(TimeZone.getDefault());
-
         try{
-            Long startEpochTime = dateFormat.parse(dateString).getTime() / 1000;
+            Long startEpochTime = fbDateFormat.parse(dateString).getTime() / 1000;
             Long endEpochTime = startEpochTime + 24*60*60;
 
             ((MultivaluedMapImpl) queryParams).add("since", Long.toString(startEpochTime));
@@ -50,21 +50,24 @@ class FacebookHelper {
         ClientResponse webResponse;
 
         if (queryParams != null){
+            ((MultivaluedMapImpl) queryParams).add("access_token", System.getenv("FB_ACCESS_TOKEN"));
             webResponse =
                     apiClient.queryParams(queryParams).accept("application/json").get(ClientResponse.class);
 
         }else{
+            ((MultivaluedMapImpl) queryParams).add("access_token", System.getenv("FB_ACCESS_TOKEN"));
             webResponse = apiClient.accept("application/json").get(ClientResponse.class);
         }
 
         if (webResponse == null || webResponse.getStatus() != 200){
+            System.out.println(webResponse);
             throw new HTTPException(webResponse.getStatus());
         }
 
         return new JSONObject(webResponse.getEntity(String.class));
     }
 
-    List<String> getFeedOnDate(String dateString) throws HTTPException, JSONException{
+    private List<String> getFeedOnDate(String dateString) throws HTTPException, JSONException{
         /*
         Given a FB user, get posts from his feed on a certain day.
          */
@@ -72,7 +75,7 @@ class FacebookHelper {
         ((MultivaluedMapImpl) queryParams).add("access_token", System.getenv("FB_ACCESS_TOKEN"));
         dateStringToDate(dateString, queryParams);
 
-        JSONObject respData = makeGetRequest(fbFeedGraphAPIURL, queryParams);
+        JSONObject respData = makeGetRequest(fbMyGraphAPIURL+"/feed", queryParams);
         JSONArray posts = respData.getJSONArray("data");
         List<String> postIds = new ArrayList<String>();
 
@@ -93,5 +96,40 @@ class FacebookHelper {
         }
 
         return postIds;
+    }
+
+    private Calendar getUserDOB() throws JSONException, NullPointerException, ParseException{
+        /*
+        Gets a user birthday from his profile
+         */
+
+        MultivaluedMapImpl queryParams = new MultivaluedMapImpl();
+        queryParams.add("fields", "birthday");
+        String birthdayString = makeGetRequest(fbMyGraphAPIURL, queryParams).getString("birthday");
+        Calendar bdayDateCal = Calendar.getInstance();
+        bdayDateCal.setTime(fbDateFormat.parse(birthdayString));
+        return bdayDateCal;
+    }
+
+    List<String> getBirthdayWishes() throws JSONException, NullPointerException, ParseException{
+        /*
+        This method identifies the user's birthday and gets posts from this year's birthday if it has already elapsed
+        (or from last years if it has not elapsed) and replies to their wish by tagging them in the post with a custom
+        message.
+         */
+
+        Calendar userBday = getUserDOB();
+        Calendar today = Calendar.getInstance();
+
+        // setting birthday to this year from date of birth
+        userBday.add(Calendar.YEAR, today.get(Calendar.YEAR) - userBday.get(Calendar.YEAR));
+
+        // getting last year's wishes
+
+        if (today.before(userBday))
+            userBday.add(Calendar.YEAR, -1);
+
+        return getFeedOnDate(fbDateFormat.format(userBday.getTime()));
+
     }
 }
