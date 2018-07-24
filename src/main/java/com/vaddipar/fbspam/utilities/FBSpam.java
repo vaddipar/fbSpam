c5f6782842164da093e252168692ce5b78f2cccd
Sandeep Srivastav Vaddiparthy <vsandeepsrivastav@gmail.com>
2018-07-24 23:55:26 +0530

    code formatting
    1. Added code formatting according to google-java-format plugin. thanks to Bharadwaj Turlapati's suggestion
    2. Restructured a lot of code.
    3. Test the time with multiple days.

--- src/main/java/com/vaddipar/fbspam/utilities/FBSpam.java
+++ src/main/java/com/vaddipar/fbspam/utilities/FBSpam.java
@@ -1,14 +1,15 @@
 package com.vaddipar.fbspam.utilities;
 
+import org.json.JSONException;
+
 import javax.xml.ws.http.HTTPException;
+import java.util.List;
 
 public class FBSpam {
-    public static void main(String args[]){
+    public static void main(String args[]) throws HTTPException, JSONException {
         FacebookHelper fbHelper = new FacebookHelper();
-        try{
-            System.out.println(fbHelper.getFeedOnDate("2016/07/21"));
-        }catch (HTTPException e){
-            System.out.println(e.getStatusCode());
-        }
+        List<String> postIds = fbHelper.getFeedOnDate("2016/07/21");
+        for (String postId : postIds) System.out.println(postId);
+        System.out.println("I fetched "+postIds.size()+" posts");
     }
 }
--- src/main/java/com/vaddipar/fbspam/utilities/FacebookHelper.java
+++ src/main/java/com/vaddipar/fbspam/utilities/FacebookHelper.java
@@ -4,6 +4,7 @@ import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.core.util.MultivaluedMapImpl;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -14,52 +15,83 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
-public class FacebookHelper {
+class FacebookHelper {
 
-    private String fbFeedGraphAPIURL = "https://graph.facebook.com/me/feed";
+    private String fbFeedGraphAPIURL;
 
-    private MultivaluedMap dateStringToDate(String dateString){
+    FacebookHelper() {
+        fbFeedGraphAPIURL = "https://graph.facebook.com/me/feed";
+    }
+
+    private void dateStringToDate(String dateString, MultivaluedMap queryParams){
         /*
         Given a string of the format YYYY/MM/DD. Convert it to a date object and return it.
          */
 
-        DateFormat dateFormat = new SimpleDateFormat("YYYY/MM/DD HH:mm:ss", Locale.ENGLISH);
-        // dateFormat.setTimeZone(TimeZone.getDefault());
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd", Locale.ENGLISH);
+        dateFormat.setTimeZone(TimeZone.getDefault());
 
-        String startTime = dateString+ " 00:00:00";
-        String endTime = dateString+ " 23:59:59";
+        try{
+            Long startEpochTime = dateFormat.parse(dateString).getTime() / 1000;
+            Long endEpochTime = startEpochTime + 24*60*60;
 
-        MultivaluedMap dayEpoch = new MultivaluedMapImpl();
-        ((MultivaluedMapImpl) dayEpoch).add("since", "1469039400");
-        ((MultivaluedMapImpl) dayEpoch).add("access_token", System.getenv("FB_ACCESS_TOKEN"));
-        ((MultivaluedMapImpl) dayEpoch).add("until", "1469125799");
-        System.out.println(dayEpoch);
-        return dayEpoch;
-        //return null;
+            ((MultivaluedMapImpl) queryParams).add("since", Long.toString(startEpochTime));
+            ((MultivaluedMapImpl) queryParams).add("until", Long.toString(endEpochTime));
+
+        }catch (ParseException e){
+            e.printStackTrace();
+        }
     }
 
-    public JSONObject getFeedOnDate(String dateString) throws HTTPException{
+    private JSONObject makeGetRequest(String url, MultivaluedMap queryParams) throws JSONException, NullPointerException{
+        Client webClient = new Client();
+        WebResource apiClient = webClient.resource(url);
+
+        ClientResponse webResponse;
+
+        if (queryParams != null){
+            webResponse =
+                    apiClient.queryParams(queryParams).accept("application/json").get(ClientResponse.class);
+
+        }else{
+            webResponse = apiClient.accept("application/json").get(ClientResponse.class);
+        }
+
+        if (webResponse == null || webResponse.getStatus() != 200){
+            throw new HTTPException(webResponse.getStatus());
+        }
+
+        return new JSONObject(webResponse.getEntity(String.class));
+    }
+
+    List<String> getFeedOnDate(String dateString) throws HTTPException, JSONException{
         /*
         Given a FB user, get posts from his feed on a certain day.
          */
-        MultivaluedMap epochTimes = dateStringToDate(dateString);
+        MultivaluedMap queryParams = new MultivaluedMapImpl();
+        ((MultivaluedMapImpl) queryParams).add("access_token", System.getenv("FB_ACCESS_TOKEN"));
+        dateStringToDate(dateString, queryParams);
 
-        Client webClient = new Client();
-        WebResource fbFeedClient = webClient.resource(fbFeedGraphAPIURL);
-        ClientResponse fbFeedResponse =
-                fbFeedClient.queryParams(epochTimes).accept("application/json").get(ClientResponse.class);
+        JSONObject respData = makeGetRequest(fbFeedGraphAPIURL, queryParams);
+        JSONArray posts = respData.getJSONArray("data");
+        List<String> postIds = new ArrayList<String>();
 
 
-        if (fbFeedResponse.getStatus() != 200){
-            throw new HTTPException(fbFeedResponse.getStatus());
+        while (posts!=null){
+            for (int postIndex = 0; postIndex < posts.length(); postIndex++) {
+                postIds.add(posts.getJSONObject(postIndex).get("id").toString());
+            }
+
+            try{
+                JSONObject pagingInfo = respData.getJSONObject("paging");
+                String nextPageURL = pagingInfo.get("next").toString();
+                respData = makeGetRequest(nextPageURL, null);
+                posts = respData.getJSONArray("data");
+            }catch (JSONException e){
+                posts = null;
+            }
         }
 
-        try{
-            JSONObject responseData = new JSONObject(fbFeedResponse.getEntity(String.class));
-            return responseData;
-        }catch (JSONException e) {
-            e.printStackTrace();
-        }
-        return null;
+        return postIds;
     }
 }
